69ee80c4baa5fc01baea68b7496d7d350b327584
Gjeev <g_jeevika@me.iitr.ac.in>
2022-10-07 21:58:43 +0530

    add files

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const morgan = require('morgan');
+const _ = require('lodash');
+
+const app = express();
+
+// connect th db here
+
+app.set("view engine","ejs");
+app.use(morgan("dev"));
+app.use(express.static("public"));
+app.use(express.urlencoded({extended: true}));
+
+app.listen(3001);
+
+app.get("/",(req,res) => {
+    res.render("index");
+});
+app.get("/about",(req,res) => {
+    res.render("about");
+});
+app.get("/contact-us",(req,res) => {
+    res.render("contact");
+});
+app.get("/people",(req,res) => {
+    res.render("people");
+});
+//middleware
+app.use((req, res) => {
+    res.render("error");
+});
+
+
