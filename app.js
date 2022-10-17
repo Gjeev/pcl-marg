a2246a1548786f47ca5af33164efd6481a07ac0e
Gjeev <g_jeevika@me.iitr.ac.in>
2022-10-17 22:20:32 +0530

    fix footer, about and join us navigation

--- app.js
+++ app.js
@@ -16,12 +16,6 @@ app.listen(3001);
 app.get("/",(req,res) => {
     res.render("index");
 });
-app.get("/about",(req,res) => {
-    res.render("about");
-});
-app.get("/contact-us",(req,res) => {
-    res.render("contact");
-});
 app.get("/people",(req,res) => {
     res.render("people");
 });
