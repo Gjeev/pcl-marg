59c3dad6f5b1dba9da5e19c6fe3cf45d5c472e14
Gjeev <g_jeevika@me.iitr.ac.in>
2022-10-19 13:39:45 +0530

    static

--- app.js
+++ app.js
@@ -1,33 +1,23 @@
-const express = require('express');
-const morgan = require('morgan');
-const _ = require('lodash');
+var bar = document.querySelectorAll(".bar");
+    var hamburgersection = document.querySelector("#hamburger_section");
+    hamburgersection.onmouseover = function () {
+      for (let j = 0; j < 3; j++) {
+        bar[j].style.height = "4px";
+        bar[j].style.width = "30px";
+      }
+    };
+    hamburgersection.onmouseout = function () {
+      for (let j = 0; j < 3; j++) {
+        bar[j].style.height = "3px";
+        bar[j].style.width = "25px";
+      }
+    };
 
-const app = express();
+    var div2 = document.querySelector("#div2");
+    hamburgersection.addEventListener("click", () => {
+      hamburgersection.classList.toggle("active");
+      div2.classList.toggle("active");
+    });
 
-// connect th db here
-
-app.set("view engine","ejs");
-app.use(morgan("dev"));
-app.use(express.static("public"));
-app.use(express.urlencoded({extended: true}));
-
-app.listen(3001);
-
-app.get("/",(req,res) => {
-    res.render("index");
-});
-app.get("/people",(req,res) => {
-    res.render("people");
-});
-app.get("/profile",(req,res) => {
-    res.render("profile");
-});
-app.get("/background",(req,res) => {
-    res.render("background");
-});
-//middleware
-app.use((req, res) => {
-    res.render("error");
-});
 
 
